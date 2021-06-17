c79adde2f8b10e1551856b6dd955279574e0007b
Pradosh <talk2cetian@gmail.com>
2021-06-17 22:01:29 +0530

    importing password

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from secret import password
+
 def print_hi(name):
     print(f'Hello,  {name}')
+
+
+def connect_to_db():
+    connector = password
 
 if __name__ == '__main__':
     print_hi("Pradosh")
