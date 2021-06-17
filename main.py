06c06a0c1400984706a22b5d8c75679afb64c736
Pradosh <talk2cetian@gmail.com>
2021-06-17 21:51:45 +0530

    We changed hi to hello and added a new line

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def print_hi(name):
-    print(f'Hi,  {name}')
+    print(f'Hello,  {name}')
 
 if __name__ == '__main__':
     print_hi("Pradosh")
     print("how are you?")
+    print("where do you leave?")
 
