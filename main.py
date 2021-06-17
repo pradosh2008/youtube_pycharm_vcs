026ca150ccfe2a41059c8971d72270b3b06ac75e
Pradosh <talk2cetian@gmail.com>
2021-06-17 22:10:45 +0530

    added sum function to dev branch

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from secret import password
 def print_hi(name):
     print(f'Hello,  {name}')
 
+def sum(a,b):
+    print(f"sum is {a+b}")
 
 def connect_to_db():
     connector = password
@@ -11,4 +13,6 @@ if __name__ == '__main__':
     print_hi("Pradosh")
     print("how are you?")
     print("where do you leave?")
+
+    sum(1,3)
 
